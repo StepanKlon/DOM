620755428ad24e9259ed84b2504285752ef38561
StepanKlon <klonstepan@gmail.com>
2021-02-12 16:33:45 +0100

    DOM

---
+++ DOM.js
@@ -0,0 +1,19 @@
+let names = ["Jitka","Marek","Jakub","Štěpán"];
+
+names.forEach(name => {
+
+    if (name === "Štěpán") {
+    $("ul").append("<li><strong>" + name + "</strong></li>"); 
+    } else {
+    $("ul").append("<li>" + name + "</li>");
+    }
+ 
+});
+
+let additionalBlock = {
+    title: "Added with javascript",
+    text: "This block was added using JavaScript's jQuery library. How awesome!"
+  };
+
+$("body").append("<h1>" + additionalBlock.title + "</h1>");
+$("body").append("<p>" + additionalBlock.text + "</p>");
